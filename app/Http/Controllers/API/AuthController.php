04c1067a2363f9c8a9ea0677dfb57e92d86b072d
mohammadkhoshnood <mohammadkhoshnood88@gmail.com>
2021-08-29 11:17:20 +0430

    create api for generate token

--- app/Http/Controllers/API/AuthController.php
+++ app/Http/Controllers/API/AuthController.php
@@ -3,9 +3,42 @@
 namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
+use App\Models\Ip;
 use Illuminate\Http\Request;
 
 class AuthController extends Controller
 {
-    //
+    public $loginAfterSignUp = true;
+
+    public function __construct()
+    {
+        auth()->setDefaultDriver('api');
+    }
+
+    public function login(Request $request)
+    {
+        $ip = $request->getClientIp();
+
+        $user = Ip::where('ip' , $ip)->first();
+
+        if (isset($user)){
+
+            $token = auth()->login($user);
+            return $this->respondWithToken($token);
+
+        }
+
+        return response('your ip not found');
+    }
+
+    protected function respondWithToken($token)
+    {
+        return response()->json([
+            'code' => "200",
+            'access_token' => $token,
+            'token_type' => 'bearer',
+            'expires_in' => auth()->factory()->getTTL()
+        ]);
+    }
+
 }
--- app/Models/Ip.php
+++ app/Models/Ip.php
@@ -3,9 +3,27 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
+use Tymon\JWTAuth\Contracts\JWTSubject;
+use Illuminate\Foundation\Auth\User as Authenticatable;
 
-class Ip extends Model
+
+
+class Ip extends Authenticatable implements JWTSubject
 {
     use HasFactory;
+
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();
+    }
+
+    /**
+     * Return a key value array, containing any custom claims to be added to the JWT.
+     *
+     * @return array
+     */
+    public function getJWTCustomClaims()
+    {
+        return [];
+    }
 }
